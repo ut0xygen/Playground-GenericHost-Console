b4f076ba78edd25f2b51ba0057f6b4cc9039320e
Yutamaro <87888896+ut0xygen@users.noreply.github.com>
2024-09-07 19:20:47 +0900

    Initial commit

---
+++ Playground-GenericHost-Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using PlaygroundGenericHostConsole.Services;
+
+var builder = Host.CreateDefaultBuilder(args);
+builder.ConfigureAppConfiguration(config => {
+    config.AddCommandLine(args);
+});
+builder.ConfigureLogging(config => {
+    // REF: https://learn.microsoft.com/ja-jp/aspnet/core/fundamentals/logging/?view=aspnetcore-8.0
+    // REF: https://learn.microsoft.com/ja-jp/dotnet/core/extensions/logging?tabs=command-line
+    // REF; https://qiita.com/TsuyoshiUshio@github/items/1ed49c90dc137d55bcfb
+    config.ClearProviders();
+    config.AddConsole();
+
+    // https://learn.microsoft.com/ja-jp/dotnet/core/extensions/console-log-formatter
+    config.AddSimpleConsole(options => {
+        options.SingleLine = true;
+        options.IncludeScopes = true;
+        options.UseUtcTimestamp = true;
+        options.TimestampFormat = "yyyy/MM/dd HH:mm:ss.ffffff ";
+    });
+});
+
+builder.ConfigureServices(config => {
+    config.AddHostedService<ConsoleApplicationHostedService>();
+});
+
+var app = builder.Build();
+
+await app.RunAsync();
---
+++ Playground-GenericHost-Console/Services/ConsoleApplicationHostedService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace PlaygroundGenericHostConsole.Services;
+
+public class ConsoleApplicationHostedService(
+    IHostEnvironment HostEnvironment,
+    IConfiguration Configuration,
+    ILogger<ConsoleApplicationHostedService> Logger,
+    IHostApplicationLifetime ApplicationLifetime
+) : IHostedService
+{
+    // REF: https://learn.microsoft.com/ja-jp/dotnet/api/system.threading.tasks.task.yield?view=net-8.0
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        ApplicationLifetime.ApplicationStarted.Register(OnStarted);
+        ApplicationLifetime.ApplicationStopping.Register(OnStopping);
+        ApplicationLifetime.ApplicationStopped.Register(OnStopped);
+
+        Logger.LogInformation(new EventId(), "Called -> StartAsync()");
+
+#if DEBUG
+        var applicationName = HostEnvironment.ApplicationName;
+        var environmentName = HostEnvironment.EnvironmentName;
+        var configurations = Configuration.AsEnumerable().ToList();
+#endif
+
+        await Task.Yield();
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        Logger.LogInformation(new EventId(), "Called -> StopAsync()");
+
+        await Task.Yield();
+    }
+
+    private async void OnStarted()
+    {
+        Logger.LogInformation("Called -> OnStarted()");
+
+        await Task.Yield();
+    }
+
+    private async void OnStopping()
+    {
+        Logger.LogInformation("Called -> OnStopping()");
+
+        await Task.Yield();
+    }
+
+    private async void OnStopped()
+    {
+        Logger.LogInformation("Called -> OnStopped()");
+
+        await Task.Yield();
+    }
+}
